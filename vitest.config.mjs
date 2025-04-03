2601f12321fbd105f87e946cb29ad37d9ca2f3b3
Yannick Silva <garciatandela@gmail.com>
2025-04-03 21:43:26 +0100

    docs: project documentation

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,15 +2,9 @@ import { defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 import jsconfigPaths from 'vite-jsconfig-paths';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
   test: {
     environment: 'happy-dom',
     setupFiles: 'test/vitest/setup-file.js',
